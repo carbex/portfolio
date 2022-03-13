7fa7fe2294c6f05bb3a24ce26ad10a57c9538521
carbex <cbx92@hotmail.fr>
2022-03-13 14:49:51 +0100

    Change portfolio card display

--- reactapp/src/components/MainView/Portfolio/Card/Body/Body.js
+++ reactapp/src/components/MainView/Portfolio/Card/Body/Body.js
@@ -1,37 +1,54 @@
-import React from 'react'
-import * as S from './Body.styles'
-import { Link } from 'react-router-dom'
+import React from "react";
+import * as S from "./Body.styles";
+import { Link } from "react-router-dom";
 
 function Body(props) {
-
   // Default props
   const {
-    description = '',
+    description = "",
     resources = [],
-    githubUrl = '',
-    siteUrl = '',
-    visible = false
-  } = props
+    githubUrl = "",
+    siteUrl = "",
+    visible = false,
+  } = props;
 
-  let resourcesList = resources.map((resource) => <li key={resource}>{resource}</li>)
+  let resourcesList = resources.map((resource) => (
+    <li key={resource}>{resource}</li>
+  ));
 
   return (
     <S.Container visible={visible}>
       <S.SubTitle>A propos du projet</S.SubTitle>
-      <S.Paragraph>
-        {description}
-      </S.Paragraph>
+      <S.Paragraph>{description}</S.Paragraph>
       <S.SubTitle>Fiche technique</S.SubTitle>
-      <S.Ul>
-        {resourcesList}
-      </S.Ul>
-      <S.SubTitle>Ressources</S.SubTitle>
-      <S.Paragraph style={{ display: 'flex', flexDirection: 'column' }}>
-        {siteUrl && <Link to={{ pathname: siteUrl }} target="_blank" style={{textDecoration: 'none'}}><S.Link>Lien du site</S.Link></Link>}
-        {githubUrl && <Link to={{ pathname: githubUrl }} target="_blank" style={{textDecoration: 'none'}}><S.Link>Lien Github</S.Link></Link>}
-      </S.Paragraph>
+      <S.Ul>{resourcesList}</S.Ul>
+      {(siteUrl || githubUrl) && (
+        <>
+          <S.SubTitle>Ressources</S.SubTitle>
+          <S.Paragraph style={{ display: "flex", flexDirection: "column" }}>
+            {siteUrl && (
+              <Link
+                to={{ pathname: siteUrl }}
+                target="_blank"
+                style={{ textDecoration: "none" }}
+              >
+                <S.Link>Lien du site</S.Link>
+              </Link>
+            )}
+            {githubUrl && (
+              <Link
+                to={{ pathname: githubUrl }}
+                target="_blank"
+                style={{ textDecoration: "none" }}
+              >
+                <S.Link>Lien Github</S.Link>
+              </Link>
+            )}
+          </S.Paragraph>
+        </>
+      )}
     </S.Container>
-  )
+  );
 }
 
-export default Body
+export default Body;
